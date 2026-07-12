2c08d2933916b746323ed633e6658196bb0d5f5f
F Tsang <chtsang26@gmail.com>
2026-07-12 22:13:09 +0100

    pipe roughless default value format to 3 decimal place

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title("Pipe Friction Calculation")
 V = st.number_input("Volume flow (l/s)", value=50.0)
 d = st.number_input("Pipe diameter (mm)", value=150.0)
 v = st.number_input("Kinematic viscosity (m2/s) water at 25 ℃", value=0.000000895, format="%.10f")
-k = st.number_input("Pipe roughness (mm), heavy grade steel pipe", value=0.046)
+k = st.number_input("Pipe roughness (mm), heavy grade steel pipe", value=0.046,  format="%.3f")
 
 if st.button("Calculate"):
     u, re, f, pd = haaland(V, d, v, k)
